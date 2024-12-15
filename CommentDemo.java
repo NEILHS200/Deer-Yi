1d860dad869b4c544b1be3bcba120b2ea65882ee
NEILHS200 <66925633+NEILHS200@users.noreply.github.com>
2024-12-15 10:21:02 -0600

    Add files via upload

---
+++ CommentDemo.java
@@ -0,0 +1,20 @@
+package com.suna.comment;
+
+/**
+ *  Today, we are gonna learn comment.
+ */
+public class CommentDemo {
+    public static void main(String[] args) {
+        //单行注释
+        //单行注释
+
+
+            /*窗前明月光
+            疑是地上霜
+            举头望明月
+            低头思故乡*/
+
+        // shortcut : ctrl + /       and     ctrl + shift + /
+
+    }
+}
