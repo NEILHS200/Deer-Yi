56d453bf7f238ab2e7603e3d2d5434db8e8a495a
NEILHS200 <66925633+NEILHS200@users.noreply.github.com>
2025-02-15 22:30:09 -0600

    Add files via upload

---
+++ Day01-Java/LiteralDemo.java
@@ -0,0 +1,26 @@
+package com.suna.literal;
+
+/**
+ *  Today, our content is literal.
+ */
+public class LiteralDemo {
+    public static void main(String[] args) {
+        // 整数 integer
+            // 666, -88, 667
+
+        // 小数 fractions
+            // 13.14, -5.21
+
+        // 字符 character
+            // A,0,我    -->    'A','0','我'
+
+        // 字符串 string
+            // suna      -->    "suna"
+
+        // 布尔值 boolean
+            // true , false
+
+        // 空值
+            // null
+    }
+}
