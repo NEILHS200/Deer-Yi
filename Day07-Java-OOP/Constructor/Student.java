320f9447785759bd0dd24126cfbbaf9aec6a1f52
NEILHS200 <66925633+NEILHS200@users.noreply.github.com>
2025-03-05 15:29:55 -0600

    add constructor files

---
+++ Day07-Java-OOP/Constructor/Student.java
@@ -0,0 +1,15 @@
+package com.yde.constructor;
+
+public class Student {
+
+    // Constructor without parameters
+    public Student(){
+        System.out.println("Non-parameters constructor is activated!");
+    }
+
+    // Constructor with parameters
+
+    public Student(String name, double score){
+        System.out.println("Constructors with parameters is activated!");
+    }
+}
---
+++ Day07-Java-OOP/Constructor/Test.java
@@ -0,0 +1,14 @@
+package com.yde.constructor;
+
+public class Test {
+    public static void main(String[] args) {
+        // I am gonna learn how to use Constructor here
+        Student s1 = new Student();
+        Student s2 = new Student("Deer", 90);
+
+        // Java has a default non-parameters constructor
+        // If we create a constructor with parameters, and we need non-parameters constructor.
+        // then we need to create it our own.
+
+    }
+}
